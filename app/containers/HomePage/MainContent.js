9814cc5e568e85c090547fb3fc2fbbdbda685f9f
alexiso <alexiso@umich.edu>
2017-01-30 21:12:20 -0500

    footer

--- app/containers/HomePage/MainContent.js
+++ app/containers/HomePage/MainContent.js
@@ -8,18 +8,12 @@ class MainContent extends React.Component {
     return (
       <Container>
         <CenteredSection>
-          <div className="col-md-4">
-            <h2>Hack Night</h2>
-            <p>Every Thursday in TechArb, everyone is invited to come work on anything and everything.</p>
-          </div>
-          <div className="col-md-4">
-            <h2>Learn to Hack</h2>
-            <p>Dedicated to teaching others a variety of intro topics such as web and mobile development</p>
-          </div>
-          <div className="col-md-4">
-            <h2>Mentorship</h2>
-            <p>lol</p>
-          </div>
+
+
+          
+          
+
+
         </CenteredSection>
       </Container>
     );
