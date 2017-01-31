110055a4076fe0562c2d363e6954bf8dcd97a032
Jason Wozniak <jasonwoz@umich.edu>
2017-01-30 21:13:43 -0500

    Added an input box

--- app/containers/HomePage/MainContent.js
+++ app/containers/HomePage/MainContent.js
@@ -1,25 +1,55 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Container from './Container';
 import CenteredSection from './CenteredSection';
 
+const SubLink = styled.button`
+  html button styles
+  backgroundColor:white;
+`;
+
+const SubInput = styled.input`
+  backgroundColor:cyan;
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+  border: 2px solid red;
+  border-radius: 4px;
+`;
+
 class MainContent extends React.Component {
+
+  constructor(){
+    super()
+    this.state = { emailText: '' }
+    this.emailChange = this.emailChange.bind(this)
+  }
+
+  emailChange(e){
+    this.setState({ emailText: e.target.value })
+  }
+
   render() {
     return (
       <Container>
         <CenteredSection>
-          <div className="col-md-4">
-            <h2>Hack Night</h2>
-            <p>Every Thursday in TechArb, everyone is invited to come work on anything and everything.</p>
+
+
+          <div className="subscribe-section">
+            <SubLink/>
+            <h2>Join Michigan Hackers</h2>
+            <p>If you're interested in more specifics about what we do, or in joining us at events:.</p>
+            <p>Click here to learn more.</p>
+            <p>Recieve our email detailing upcoming upcoming events, tech talks, hack-athons, and news once every few weeks:</p>
+            <form>  
+                <SubInput type="text" value={this.state.emailText} onChange={(e)=>{this.emailChange(e)}} />
+            </form>
           </div>
-          <div className="col-md-4">
-            <h2>Learn to Hack</h2>
-            <p>Dedicated to teaching others a variety of intro topics such as web and mobile development</p>
-          </div>
-          <div className="col-md-4">
-            <h2>Mentorship</h2>
-            <p>lol</p>
-          </div>
+        
+
+
         </CenteredSection>
       </Container>
     );
@@ -27,3 +57,4 @@ class MainContent extends React.Component {
 }
 
 export default MainContent;
+
