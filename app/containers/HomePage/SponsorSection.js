bd5f666f69da907e6fc2f15e3c039e3dc7dcb58c
andrewbo <andrewbo@umich.edu>
2017-02-07 23:04:37 -0500

    Added image files and put them in sponsor section

--- app/containers/HomePage/SponsorSection.js
+++ app/containers/HomePage/SponsorSection.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
+import MicrosoftLogo from 'static/assets/sponsors/microsoft-logo.svg';
+import CapitalOneLogo from 'static/assets/sponsors/capital-one-logo.svg';
+import ArmLogo from 'static/assets/sponsors/ARM_logo.svg';
+import FacebookLogo from 'static/assets/sponsors/facebook-logo.png';
+import QualtricsLogo from 'static/assets/sponsors/qualtrics-logo.jpg';
+import GoogleLogo from 'static/assets/sponsors/google-logo.svg';
+import YelpLogo from 'static/assets/sponsors/yelp-logo.svg';
 
-const SponsorImage = styled.div`
-    width: 300px;
-    height: 100px;
-    margin: 5px;
-    backgroundColor: black;
+const SponsorImage = styled.img`
+  vertical-align: middle;
+  max-height: 100%;
+  max-width: 100%;
+  margin: 15px;
+`;
+
+const ImageWrapper = styled.div`
+  height: 100%;
+  width: 33%;
+  padding: 15px;
 `;
 
 const SponsorRow = styled.div`
-    display: flex;
-    justifyContent: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 class SponsorSection extends React.Component { // eslint-disable-line react/prefer-stateless-function
@@ -21,20 +35,35 @@ class SponsorSection extends React.Component { // eslint-disable-line react/pref
         <p>Blurb about the sponsors but idk what to put because I am a p.o.s!</p>
 
         <SponsorRow>
-          <SponsorImage><p>Google</p></SponsorImage>
-          <SponsorImage><p>Facebook</p></SponsorImage>
+          <ImageWrapper>
+            <SponsorImage src={MicrosoftLogo}></SponsorImage>
+          </ImageWrapper>
+          <ImageWrapper>
+            <SponsorImage src={CapitalOneLogo}></SponsorImage>
+          </ImageWrapper>
         </SponsorRow>
 
         <SponsorRow>
-          <SponsorImage><p>Microsoft</p></SponsorImage>
-          <SponsorImage><p>Capital One</p></SponsorImage>
-          <SponsorImage><p>ARM</p></SponsorImage>
+          <ImageWrapper>
+            <SponsorImage src={ArmLogo}></SponsorImage>
+          </ImageWrapper>
+          <ImageWrapper>
+            <SponsorImage src={FacebookLogo}></SponsorImage>
+          </ImageWrapper>
+          <ImageWrapper>
+            <SponsorImage src={QualtricsLogo}></SponsorImage>
+          </ImageWrapper>
         </SponsorRow>
 
         <SponsorRow>
-          <SponsorImage><p>Qualtrics</p></SponsorImage>
-          <SponsorImage><p>Yelp</p></SponsorImage>
+          <ImageWrapper>
+            <SponsorImage src={GoogleLogo}></SponsorImage>
+          </ImageWrapper>
+          <ImageWrapper>
+            <SponsorImage src={YelpLogo}></SponsorImage>
+          </ImageWrapper>
         </SponsorRow>
+
       </div>
 
     );
