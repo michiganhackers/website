94f1f38dc5ab7d6cd9d187d56bc6d0dd7e727c80
andrewbo <andrewbo@umich.edu>
2017-02-08 02:09:34 -0500

    Fixed sponsor image layout and ARM logo

--- app/containers/HomePage/SponsorSection.js
+++ app/containers/HomePage/SponsorSection.js
@@ -5,23 +5,17 @@ import Container from 'components/Container';
 
 import MicrosoftLogo from 'static/assets/sponsors/microsoft-logo.svg';
 import CapitalOneLogo from 'static/assets/sponsors/capital-one-logo.svg';
-import ArmLogo from 'static/assets/sponsors/ARM_logo.svg';
+import ArmLogo from 'static/assets/sponsors/arm-logo.svg';
 import FacebookLogo from 'static/assets/sponsors/facebook-logo.png';
 import QualtricsLogo from 'static/assets/sponsors/qualtrics-logo.jpg';
 import GoogleLogo from 'static/assets/sponsors/google-logo.svg';
 import YelpLogo from 'static/assets/sponsors/yelp-logo.svg';
 
 const SponsorImage = styled.img`
-  vertical-align: middle;
-  max-height: 100%;
-  max-width: 100%;
-  margin: 15px;
-`;
-
-const ImageWrapper = styled.div`
   height: 100%;
   width: 33%;
   padding: 15px;
+  max-width: 100%;
 `;
 
 const SponsorRow = styled.div`
@@ -38,33 +32,19 @@ class SponsorSection extends React.Component { // eslint-disable-line react/pref
         <p>Blurb about the sponsors but idk what to put because I am a p.o.s!</p>
 
         <SponsorRow>
-          <ImageWrapper>
-            <SponsorImage src={MicrosoftLogo}></SponsorImage>
-          </ImageWrapper>
-          <ImageWrapper>
-            <SponsorImage src={CapitalOneLogo}></SponsorImage>
-          </ImageWrapper>
+          <SponsorImage src={MicrosoftLogo}></SponsorImage>
+          <SponsorImage src={CapitalOneLogo}></SponsorImage>
         </SponsorRow>
 
         <SponsorRow>
-          <ImageWrapper>
-            <SponsorImage src={ArmLogo}></SponsorImage>
-          </ImageWrapper>
-          <ImageWrapper>
-            <SponsorImage src={FacebookLogo}></SponsorImage>
-          </ImageWrapper>
-          <ImageWrapper>
-            <SponsorImage src={QualtricsLogo}></SponsorImage>
-          </ImageWrapper>
+          <SponsorImage src={ArmLogo}></SponsorImage>
+          <SponsorImage src={FacebookLogo}></SponsorImage>
+          <SponsorImage src={QualtricsLogo}></SponsorImage>
         </SponsorRow>
 
         <SponsorRow>
-          <ImageWrapper>
-            <SponsorImage src={GoogleLogo}></SponsorImage>
-          </ImageWrapper>
-          <ImageWrapper>
-            <SponsorImage src={YelpLogo}></SponsorImage>
-          </ImageWrapper>
+          <SponsorImage src={GoogleLogo}></SponsorImage>
+          <SponsorImage src={YelpLogo}></SponsorImage>
         </SponsorRow>
 
       </Container>
