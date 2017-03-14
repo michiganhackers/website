198bdbdf489b8a4ae0b55feb116810e7d26e4e6d
Andrew Boland <andrewbo@umich.edu>
2017-03-13 20:02:37 -0400

    Added test button

--- app/containers/HomePage/KnowledgeBaseSection.js
+++ app/containers/HomePage/KnowledgeBaseSection.js
@@ -23,6 +23,21 @@ const KnowledgeBaseButton = styled.a`
   font-size: 20px;
 `;
 
+const HeaderButton = styled.button`
+  width: 25%;
+  height: 40px;
+  color: white;
+  background: #ef5f32;
+  border-radius: 5px;
+  font-size: 16px;
+  text-transform: uppercase;
+  font-weight: bold;
+  font-size: 20px;
+  display: flex;
+  justify-content: center;
+  margin: 0 auto;
+`;
+
 const KnowledgeBaseSectionWrapper = styled.div`
   padding: 70px 0;
 `;
@@ -35,6 +50,7 @@ class KnowledgeBaseSection extends React.Component { // eslint-disable-line reac
           <SectionHeader text="Knowledge Base" darkBackground={true} />
           <ContentText>Being a michigan hacker means you want something more out of your Michigan experience. Lorem I cant even see the tacos from my residence in the highest
             apartment in the city of which I have never seen nor know the location</ContentText>
+          <a href="google.com"><HeaderButton>Test</HeaderButton></a>
           <KnowledgeBaseButton href="https://www.google.com">Direct Link</KnowledgeBaseButton>
         </Container>
       </KnowledgeBaseSectionWrapper>
